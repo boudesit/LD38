cfa97210530b609ede10ad985b56702d2e221e82
TGuillaume <tristan.guillaume-ISC_France@fr.ibm.com>
2017-04-22 17:08:29 +0200

    DINDON

--- js/unity/UnityManager.js
+++ js/unity/UnityManager.js
@@ -1,4 +1,8 @@
 function UnityManager(game) {
+	this.game = game;
+	this.unitType = null;
+	this.iscreate = false;
+	this.unitBattle = null;
 };
 
 UnityManager.prototype.create = function create() {
@@ -7,4 +11,19 @@ UnityManager.prototype.create = function create() {
 };
 
 UnityManager.prototype.update = function update() {
+	if(this.unitType != null && this.iscreate === true) {
+
+		this.unitBattle = new UnitBattle(this.game, this.unitType);
+		this.unitBattle.create();
+		this.iscreate = false;
+	}
+};
+
+UnityManager.prototype.setUnitType = function setUnitType(unitType) {
+	this.unitType = unitType;
+};
+
+UnityManager.prototype.isCreate = function isCreate(isCreate) {
+
+	this.iscreate = isCreate;
 };
