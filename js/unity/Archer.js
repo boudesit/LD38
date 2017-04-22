23e34e9a9cd59e09b82cd4daff462ad9b45eaced
TGuillaume <tristan.guillaume-ISC_France@fr.ibm.com>
2017-04-22 17:41:01 +0200

    y'a des canards

--- js/unity/Archer.js
+++ js/unity/Archer.js
@@ -1,15 +1,36 @@
-function Archer(game) {
-	this.life = 1;
+function Archer(game, unitClans) {
+	this.game = game;
+	this.life = 4;
+	this.archerSprite = null;
   this.dead = false;
+	if(unitClans === "player") {
+		this.velocityX = 20;
+		this.posX = 100;
+	} else {
+		this.velocityX = -20;
+		this.posX = 700;
+	}
 };
 
 Archer.prototype.create = function create() {
 
-	console.log("coucou je suis un archer");
+	this.archerSprite = this.game.add.sprite(this.posX, 400, "Canard", 0);
+	this.archerSprite.animations.add('idle', [0,1,2]);
+	this.archerSprite.animations.play('idle', 5, true);
+	this.game.physics.arcade.enable(this.archerSprite);
+	this.archerSprite.enableBody = true;
+	this.archerSprite.body.velocity.x = this.velocityX;
+	this.archerSprite.body.velocity.y = 0;
+	this.archerSprite.physicsBodyType = Phaser.Physics.ARCADE;
+	this.archerSprite.body.collideWorldBounds=true;
+	this.archerSprite.anchor.set(0.5);
+	this.archerSprite.scale.setTo(2,2);
+	this.archerSprite.life = this.life;
+	this.archerSprite.damage = 1;
 };
 
 Archer.prototype.getSprite = function getSprite() {
-	return this.ArcherSprite;
+	return this.archerSprite;
 };
 
 Archer.prototype.update = function update() {
