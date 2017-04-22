55471ae5c5d7633dbb7fad93c8ac4068acc3a59c
TGuillaume <tristan.guillaume-ISC_France@fr.ibm.com>
2017-04-23 00:37:46 +0200

    Alllezzzzz

--- js/HUD.js
+++ js/HUD.js
@@ -77,6 +77,10 @@ HUD.prototype.update = function update() {
 	game.physics.arcade.overlap(  this.player.getPlayerUnitGroup() , this.computer.getComputerUnitGroup() , this.computeFight, null, this);
 	//
 
+	if(this.fight != null && this.fight.isFinnish()) {
+		this.player.getPlayerUnitGroup().setAll("body.velocity.x", 60);
+		this.computer.getComputerUnitGroup().setAll("body.velocity.x", -60);
+	}
 
   //  this.wavesManager.update();
   //  this.enemy.update();
