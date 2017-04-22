3e6c851a07d5ffb91c0aa7f83868344fd394b529
TGuillaume <tristan.guillaume-ISC_France@fr.ibm.com>
2017-04-22 15:30:33 +0200

    fix

--- js/unity/UnitBattle.js
+++ js/unity/UnitBattle.js
@@ -42,7 +42,7 @@ Enemy.prototype.chooseDoor = function chooseDoor(){
 	}else if(this.door === "door4"){
 		return door4;
 	}
-}
+};
 
 Enemy.prototype.destroy = function destroy() {
 	this.enemy.destroy();
