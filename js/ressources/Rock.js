4c65989fc4dcdbb525bd6bf217e003057dc72640
TGuillaume <tristan.guillaume-ISC_France@fr.ibm.com>
2017-04-24 00:15:16 +0200

    modify rand ressources

--- js/ressources/Rock.js
+++ js/ressources/Rock.js
@@ -1,38 +1,16 @@
-function Rock(game) {
+function Rock(game, posX, posY) {
 	this.game = game;
   this.rockSprite = null;
-  this.randomPosX = null;
+  this.posX = posX;
+	this.posY = posY;
 };
 
 Rock.prototype.create = function create() {
 
-
-    this.rand = this.getRandomIntInclusive(0 , 2);
-
-    if( this.rand === 0 )
-    {
-      this.randomPosX = 190
-    }
-    else if(this.rand === 1 )
-    {
-      this.randomPosX = 70
-    }
-    else
-    {
-      this.randomPosX = 300
-    }
-
-
-  	this.rockSprite = this.game.add.sprite(this.randomPosX, 535, "rock", 0);
+  	this.rockSprite = this.game.add.sprite(this.posX, this.posY, "rock", 0);
 };
 
 Rock.prototype.update = function update() {
 
 
-};
-
-Rock.prototype.getRandomIntInclusive = function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min +1)) + min;
 };
